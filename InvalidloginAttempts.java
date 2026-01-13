75a5bb2d7043851b270695ebdbe3ea66ecb277d0
Utkarsh Upadhyay <utkarshupadhayay77@gmail.com>
2026-01-13 13:23:16 +0530

    Update InvalidloginAttempts.java

--- InvalidloginAttempts.java
+++ InvalidloginAttempts.java
@@ -1,44 +1,39 @@
-package assignment;
+package Assignment120126;
+
 import java.util.Scanner;
 
-public class InvalidloginAttempts {
+public class Login {
 
 	public static void main(String[] args) {
-		
-		// 1] Crating an  Scanner class
 		Scanner sc = new Scanner(System.in);
 		
-	
-		
-		int correctpass=1807;
-		
-			
-		// 3] Main logic and try and catch block
-		
-	     for(int i=0;i<3;i++) {
-	        	System.out.println("Enter The Password :- ");
-	            int pass=sc.nextInt();
-	        	try {
-	        		if(i==2) {
-	        		if(pass!=correctpass) {
-	        			throw new LoginAttemptExceededException("LoginAttemptExceedException");
-	        		}
-	        		System.out.println("Access Granted");
-	        		}
-	        	}
-	        	catch(LoginAttemptExceededException ob) {
-	        		ob.printStackTrace();
-	        	}
-	        }
-	        sc.close();
-	        
+		 int correctpassword = 1357;
+		 
+		 for(int i=0;i<3;i++) {
+		 System.out.println("Enter password");
+		 int login = sc.nextInt();
+		try {
+			if(i==2) {
+				if(login!=correctpassword) {
+					throw new LoginAttemptExceededException("Account Locked");
+					
+				}
+			}
+			if(login==correctpassword) {
+				System.out.println("Access Granded");
+				break;
+			}
+			 
 		}
-
+		catch(LoginAttemptExceededException ob) {
+			ob.printStackTrace();
+		}
 	}
-	// 2] Creating an custom Exception
-	 class LoginAttemptExceededException extends RuntimeException{
-		 public LoginAttemptExceededException(String msg) {
-			 super(msg);
-		 }
-	 }
-
+		
+	}
+}
+class LoginAttemptExceededException extends RuntimeException{
+	public LoginAttemptExceededException(String msg) {
+		super (msg);
+	}
+}
